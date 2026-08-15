b97f546f7c61cd43c336fc05748d58f3e0c03432
Dante Grieco <dantegrieco11@gmail.com>
2026-08-14 23:08:17 -0700

    add gruvbox-inspired minimal variant

---
+++ colors/slabaster-gruvbox.lua
@@ -0,0 +1,30 @@
+local p = {
+  bg          = "#1d2021",
+  bg_alt      = "#3c3836",
+  bg_float    = "#504945",
+  bg_cursor   = "#3c3836",
+  bg_select   = "#665c54",
+  bg_visual   = "#665c54",
+  border      = "#665c54",
+  fg          = "#ebdbb2",
+  fg_dim      = "#a89984",
+  fg_faint    = "#7c6f64",
+  cursor      = "#ebdbb2",
+
+  comment     = "#e2cb5a",
+  string      = "#8ec07c",
+  number      = "#d3869b",
+  keyword     = "#fb4934",
+  type        = "#ebdbb2",
+  builtin     = "#fabd2f",
+  definition  = "#83a598",
+
+  red         = "#fb4934",
+  green       = "#b8bb26",
+  yellow      = "#fabd2f",
+  blue        = "#83a598",
+  cyan        = "#8ec07c",
+  gray        = "#928374",
+}
+
+require("slabaster.theme").load("slabaster-gruvbox", p)
